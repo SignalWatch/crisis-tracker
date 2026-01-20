f9151c0351895d103480c19715a39c9a1946f585
SignalWatch <signalwatch@example.com>
2026-01-19 21:04:28 -0800

    revert x2

--- pages/index.js
+++ pages/index.js
@@ -861,32 +861,18 @@ export default function Home() {
 
   return (
     <div style={{
+        maxWidth: 1000,
+        margin: "0 auto",
+        padding: 20,
         fontFamily: "Arial, sans-serif",
         backgroundImage: "url('/nasa.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        backgroundAttachment: "fixed",
         minHeight: "100vh",
-        position: "relative"
-      }}>
-      <div style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: "rgba(0, 0, 0, 0.3)",
-        zIndex: 0
-      }}></div>
-      <div style={{
-        maxWidth: 1000,
-        margin: "0 auto",
-        padding: 20,
         color: "#fff",
         position: "relative",
-        zIndex: 1
-      }}> 
-      </div>
+        overflow: "hidden"
+      }}>
 
         <header style={{ textAlign: "center", marginBottom: 20 }}>
           <h1 style={{
